81afcc31014183174da2a940bacdaed0b94154c9
jidealakija <francisalakija@gmail.com>
2023-11-16 16:01:45 +0100

    new commit

--- Univelcity/Univelcity/settings.py
+++ Univelcity/Univelcity/settings.py
@@ -122,6 +122,8 @@ LOGIN_URL = 'login'
 
 LOGIN_REDIRECT_URL = '/'
 
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- Univelcity/backend/urls.py
+++ Univelcity/backend/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import register, homepage, login
+from .views import register, homepage, login,logout
 
 
 urlpatterns = [
     path('', homepage, name='home'),
     path('register', register, name='register'),
-    path('login', login, name='login')
+    path('login', login, name='login'),
+    path('logout', logout, name='logout')
 ]
